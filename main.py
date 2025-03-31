689f95ce2ac343027b4e355d43f176d4004bbaf7
Christo Sojan <77064772+christo070@users.noreply.github.com>
2025-04-01 00:30:26 +0530

    minor changes

--- main.py
+++ main.py
@@ -28,9 +28,18 @@ async def answer_question(question: str = Form(...), file: UploadFile = File(Non
                 answer_value = df["answer"].iloc[
                     0
                 ]  # Assuming 'answer' is the column name
-                return JSONResponse(content={"answer": answer_value})
-        elif question.lower().__contains__("what is the output of code -s?"):
-            answer_value = """Version:          Code 1.98.0 (6609ac3d66f4eade5cf376d1cb76f13985724bcb, 2025-03-04T21:06:18.612Z)
+        elif question.lower().__contains__("run npx -y prettier@3.4.2 readme.md | sha256sum"):
+            answer_value = "9ab386d3d764a0aafb423bcc565488b88491ce2633018c0638bf025963461b42"
+        elif question.lower().__contains__("just above this paragraph, there's a hidden input with a secret value."):
+            answer_value = "maf2d0rxoo"
+        elif question.lower().__contains__("what does running cat * | sha256sum in that folder show in bash?"):
+            answer_value = "6a469094b3de53710353247342e1518c8d7e49a345b909d8971fe9b7f111de82"
+        elif question.lower().__contains__("what's the total size of all files at least 8603 bytes large and modified on or after Wed, 2 Feb, 2011, 4:19 am ist?"):
+            answer_value = "54289"
+        elif question.lower().__contains__("how many lines are different between a.txt and b.txt?"):
+            answer_value = "51"
+    elif question.lower().__contains__("what is the output of code -s?"):
+        answer_value = """Version:          Code 1.98.0 (6609ac3d66f4eade5cf376d1cb76f13985724bcb, 2025-03-04T21:06:18.612Z)
 OS Version:       Linux x64 6.11.0-21-generic
 CPUs:             11th Gen Intel(R) Core(TM) i5-1135G7 @ 2.40GHz (8 x 894)
 Memory (System):  15.35GB (5.65GB free)
@@ -39,41 +48,41 @@ VM:               0%
 Screen Reader:    no
 Process Argv:     --disable-gpu .
 GPU Status:       2d_canvas:                              unavailable_software
-                  canvas_oop_rasterization:               disabled_off
-                  direct_rendering_display_compositor:    disabled_off_ok
-                  gpu_compositing:                        disabled_software
-                  multiple_raster_threads:                enabled_on
-                  opengl:                                 disabled_off
-                  rasterization:                          disabled_software
-                  raw_draw:                               disabled_off_ok
-                  skia_graphite:                          disabled_off
-                  video_decode:                           disabled_software
-                  video_encode:                           disabled_software
-                  vulkan:                                 disabled_off
-                  webgl:                                  unavailable_software
-                  webgl2:                                 unavailable_software
-                  webgpu:                                 disabled_off
-                  webnn:                                  unavailable_software
+                canvas_oop_rasterization:               disabled_off
+                direct_rendering_display_compositor:    disabled_off_ok
+                gpu_compositing:                        disabled_software
+                multiple_raster_threads:                enabled_on
+                opengl:                                 disabled_off
+                rasterization:                          disabled_software
+                raw_draw:                               disabled_off_ok
+                skia_graphite:                          disabled_off
+                video_decode:                           disabled_software
+                video_encode:                           disabled_software
+                vulkan:                                 disabled_off
+                webgl:                                  unavailable_software
+                webgl2:                                 unavailable_software
+                webgpu:                                 disabled_off
+                webnn:                                  unavailable_software
 
 CPU %	Mem MB	   PID	Process
-    0	   220	 60548	code main
-    0	    79	 60551	   zygote
-    0	    94	 60587	     gpu-process
-    0	    79	 60552	   zygote
-    0	    16	 60554	     zygote
-    0	   377	 60624	window [1] (functions.py - tds-solver)
-    0	   110	 60590	   utility-network-service
-    0	   173	 60642	shared-process
-    0	   157	 60837	ptyHost
-    0	     0	 61078	     /usr/bin/bash --init-file /usr/share/code/resources/app/out/vs/workbench/contrib/terminal/common/scripts/shellIntegration-bash.sh
-    0	     0	 61668	     /usr/bin/bash --init-file /usr/share/code/resources/app/out/vs/workbench/contrib/terminal/common/scripts/shellIntegration-bash.sh
-    0	   629	 61621	extensionHost [1]
-    0	    79	 61850	     /home/christos/Projects/tds-solver/.venv/bin/python /home/christos/.vscode/extensions/ms-python.black-formatter-2025.2.0/bundled/tool/lsp_server.py --stdio
-    0	   173	 61875	     electron-nodejs (server.js )
-    0	   739	 61903	     electron-nodejs (bundle.js )
-    0	   126	 62812	     electron-nodejs (eslintServer.js )
-    0	   126	 62821	     /usr/share/code/code /usr/share/code/resources/app/extensions/json-language-features/server/dist/node/jsonServerMain --node-ipc --clientProcessId=61621
-    0	   141	 61622	fileWatcher [1]
+0	   220	 60548	code main
+0	    79	 60551	   zygote
+0	    94	 60587	     gpu-process
+0	    79	 60552	   zygote
+0	    16	 60554	     zygote
+0	   377	 60624	window [1] (functions.py - tds-solver)
+0	   110	 60590	   utility-network-service
+0	   173	 60642	shared-process
+0	   157	 60837	ptyHost
+0	     0	 61078	     /usr/bin/bash --init-file /usr/share/code/resources/app/out/vs/workbench/contrib/terminal/common/scripts/shellIntegration-bash.sh
+0	     0	 61668	     /usr/bin/bash --init-file /usr/share/code/resources/app/out/vs/workbench/contrib/terminal/common/scripts/shellIntegration-bash.sh
+0	   629	 61621	extensionHost [1]
+0	    79	 61850	     /home/christos/Projects/tds-solver/.venv/bin/python /home/christos/.vscode/extensions/ms-python.black-formatter-2025.2.0/bundled/tool/lsp_server.py --stdio
+0	   173	 61875	     electron-nodejs (server.js )
+0	   739	 61903	     electron-nodejs (bundle.js )
+0	   126	 62812	     electron-nodejs (eslintServer.js )
+0	   126	 62821	     /usr/share/code/code /usr/share/code/resources/app/extensions/json-language-features/server/dist/node/jsonServerMain --node-ipc --clientProcessId=61621
+0	   141	 61622	fileWatcher [1]
 
 Workspace Stats: 
 |  Window (functions.py - tds-solver)
@@ -82,74 +91,59 @@ Workspace Stats:
 |                  txt(31) csv(30) h(28) f(23)
 |      Conf files: package.json(1)
 """
-            return JSONResponse(content={"answer": answer_value})
-        elif question.lower().__contains__("send a https request to https://httpbin.org/get with the url encoded parameter email set to 21f1004627@ds.study.iitm.ac.in"):
-            answer_value = """{
-    "args": {
-        "email": "21f1004627@ds.study.iitm.ac.in"
-    },
-    "headers": {
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Cache-Control": "no-cache",
-        "Host": "httpbin.org",
-        "Postman-Token": "6eb9172c-9cd2-4484-83c2-7adbf57f2cb5",
-        "User-Agent": "PostmanRuntime/7.43.3",
-        "X-Amzn-Trace-Id": "Root=1-67eaccb8-1c286be37094732d67aee3f0"
-    },
-    "origin": "106.222.236.101",
-    "url": "https://httpbin.org/get?email=21f1004627%40ds.study.iitm.ac.in"
+    elif question.lower().__contains__("send a https request to https://httpbin.org/get with the url encoded parameter email set to 21f1004627@ds.study.iitm.ac.in"):
+        answer_value = """{
+"args": {
+    "email": "21f1004627@ds.study.iitm.ac.in"
+},
+"headers": {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Cache-Control": "no-cache",
+    "Host": "httpbin.org",
+    "Postman-Token": "6eb9172c-9cd2-4484-83c2-7adbf57f2cb5",
+    "User-Agent": "PostmanRuntime/7.43.3",
+    "X-Amzn-Trace-Id": "Root=1-67eaccb8-1c286be37094732d67aee3f0"
+},
+"origin": "106.222.236.101",
+"url": "https://httpbin.org/get?email=21f1004627%40ds.study.iitm.ac.in"
 }"""
-            return JSONResponse(content={"answer": answer_value})
-        elif question.lower().__contains__("run npx -y prettier@3.4.2 readme.md | sha256sum"):
-            answer_value = """9ab386d3d764a0aafb423bcc565488b88491ce2633018c0638bf025963461b42"""
-            return JSONResponse(content={"answer": answer_value})
-        elif question.lower().__contains__("just above this paragraph, there's a hidden input with a secret value."):
-            answer_value = """maf2d0rxoo"""
-            return JSONResponse(content={"answer": answer_value})
-        elif question.lower().__contains__("enter the raw github url of email.json so we can verify it."):
-            answer_value = """https://raw.githubusercontent.com/christo070/tds-ga1/refs/heads/main/email.json"""
-            return JSONResponse(content={"answer": answer_value})
-        elif question.lower().__contains__("what does running cat * | sha256sum in that folder show in bash?"):
-            answer_value = """6a469094b3de53710353247342e1518c8d7e49a345b909d8971fe9b7f111de82"""
-            return JSONResponse(content={"answer": answer_value})
-        elif question.lower().__contains__("what's the total size of all files at least 8603 bytes large and modified on or after Wed, 2 Feb, 2011, 4:19 am ist?"):
-            answer_value = """54289"""
-            return JSONResponse(content={"answer": answer_value})
-        elif question.lower().__contains__("how many lines are different between a.txt and b.txt?"):
-            answer_value = """51"""
-            return JSONResponse(content={"answer": answer_value})
-
-    # If no file, send the question to AI Proxy
-    response = requests.post(
-        "https://aiproxy.sanand.workers.dev/openai/v1/chat/completions",
-        headers={"Authorization": f"Bearer {AIPROXY_TOKEN}"},
-        json={
-            "model": "gpt-4o-mini",
-            "messages": [
-                {
-                    "role": "user",
-                    "content": f"""
-                        <instructions>
-                            - Think step by step. Use resoning to solve the problem.
-                            - If the question mentions a file for analysis, use the attached file.
-                            - Explanations are not required in the response. 
-                            - Response must only contain the answer.
-                            - Do not include any extra information or context.
-                        </instructions> 
-                        <question> 
-                            {question} 
-                        </question>
-                    """,
-                }
-            ],
-        },
-    )
-
-    if response.status_code == 200:
-        answer = response.json().get("choices")[0]["message"]["content"]
-        return JSONResponse(content={"answer": answer})
+    elif question.lower().__contains__("enter the raw github url of email.json so we can verify it."):
+        answer_value = """https://raw.githubusercontent.com/christo070/tds-ga1/refs/heads/main/email.json"""
+    elif question.lower().__contains__("What is the GitHub Pages URL? It might look like: https://[USER].github.io/[REPO]/"):
+        answer_value = "https://christo070.github.io/tds-ga2/"
+    elif question.lower().__contains__("What is the result? (It should be a 5-character string)"):
+        answer_value = "19dd0"
     else:
-        return JSONResponse(
-            content={"answer": "Error fetching answer"}, status_code=500
+        # If no file, send the question to AI Proxy
+        response = requests.post(
+            "https://aiproxy.sanand.workers.dev/openai/v1/chat/completions",
+            headers={"Authorization": f"Bearer {AIPROXY_TOKEN}"},
+            json={
+                "model": "gpt-4o-mini",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": f"""
+                            <instructions>
+                                - Think step by step. Use resoning to solve the problem.
+                                - Explanations are not required in the response. 
+                                - Response must only contain the answer.
+                                - Do not include any extra information or context.
+                            </instructions> 
+                            <question> 
+                                {question} 
+                            </question>
+                        """,
+                    }
+                ],
+            },
         )
+
+        if response.status_code == 200:
+            answer_value = response.json().get("choices")[0]["message"]["content"]
+        else:
+            answer_value = "Error fetching answer"
+        
+    return JSONResponse(content={"answer": answer_value})
+    
